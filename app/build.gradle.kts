9672581d2dcc1ed2d57f1ffb5fcf2469ee280217
Infi <dbrk30aa@gmail.com>
2023-09-26 11:47:02 +0300

    per rabota

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.knopka"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.knopka"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+
+}
